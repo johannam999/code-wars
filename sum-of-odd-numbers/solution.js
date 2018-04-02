ce7b58a82b5d79acbfd613f454fee93ca6f9973b
Joanna Coll <johannam999@gmail.com>
2018-04-01 22:44:30 -0700

    changes

--- sum-of-odd-numbers/solution.js
+++ sum-of-odd-numbers/solution.js
@@ -3,5 +3,8 @@ function cubeOdd(arr) {
   for (let i=0; i<arr.length; i++){
     if (typeof arr[i]==="string"){return undefined;}
   }
-  return arr.filter(x => x%2).map(x => x*x*x).reduce((acc, curr)=> acc + curr);
+  let newArr = arr.filter(x => x%2).map(x => x*x*x);
+  if (newArr.length ===0){return 0;}
+  else{
+  return newArr.reduce((acc, curr)=> acc + curr);}
   }
